6d0418d45ab1425edfd090bf998e9c6cb5bd3662
Katie Vogel <kvog983@gmail.com>
2021-03-07 18:09:52 -0500

    switched from if to cond

--- src/katie_e_vogel_main/game_logic.cljs
+++ src/katie_e_vogel_main/game_logic.cljs
@@ -8,21 +8,21 @@
 ;             :cowboy [:ninja :bear :cowboy]
 ;             :ninja [:bear :cowboy :ninja]}))
 
-(defn game-action [p1-input p2-input]
-  (if (and (= p1-input :bear) (= p2-input :ninja)) (println "Bear bites ninja! Player 1 wins!"))
-  (if (and (= p1-input :bear) (= p2-input :cowboy)) (println "Cowboy shoots bear! Player 2 wins!"))
-  (if (and (= p1-input :bear) (= p2-input :bear)) (println "It's a tie!"))
-  (if (and (= p1-input :ninja) (= p2-input :bear)) (println "Bear bites ninja! Player 2 wins!"))
-  (if (and (= p1-input :ninja) (= p2-input :cowboy)) (println "Ninja strikes cowboy! Player 1 wins!"))
-  (if (and (= p1-input :ninja) (= p2-input :ninja)) (println "It's a tie!"))
-  (if (and (= p1-input :cowboy) (= p2-input :bear)) (println "Cowboy shoots bear! Player 1 wins!"))
-  (if (and (= p1-input :cowboy) (= p2-input :ninja)) (println "Ninja strikes cowboy! Player 2 wins!"))
-  (if (and (= p1-input :cowboy) (= p2-input :cowboy)) (println "It's a tie!")))
-
-
+(defn game-action [p1-choice p2-choice]
+  (cond
+    (and (= p1-choice :bear) (= p2-choice :ninja)) :p1
+    (and (= p1-choice :bear) (= p2-choice :cowboy)) :p2
+    (and (= p1-choice :bear) (= p2-choice :bear)) :tie
+    (and (= p1-choice :ninja) (= p2-choice :bear)) :p2
+    (and (= p1-choice :ninja) (= p2-choice :cowboy)) :p1
+    (and (= p1-choice :ninja) (= p2-choice :ninja)) :tie
+    (and (= p1-choice :cowboy) (= p2-choice :bear)) :p1
+    (and (= p1-choice :cowboy) (= p2-choice :ninja)) :p2
+    (and (= p1-choice :cowboy) (= p2-choice :cowboy)) :tie))
 
 (comment
   (game-action :bear :ninja)
-  (game-action :bear :cowboy))
+  (game-action :bear :cowboy)
+  (game-action :bear :bear))
 
 
