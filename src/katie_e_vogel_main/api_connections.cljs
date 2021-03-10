472f779ad692fab4ae6a48bfceaa83576d862a31
Katie Vogel <kvog983@gmail.com>
2021-03-09 22:35:29 -0500

    successful connection to giphy API

--- src/katie_e_vogel_main/api_connections.cljs
+++ src/katie_e_vogel_main/api_connections.cljs
@@ -1,36 +1,44 @@
-;(ns katie-e-vogel-main.api-connections
-;  (:require [ajax.core :refer [GET]]
-;            [goog.dom :as gdom]
-;            [oops.core :refer [ocall oget oset!]]
-;            [hiccups.runtime :as hiccups]))
+(ns katie-e-vogel-main.api-connections
+  (:require [ajax.core :refer [GET]]))
 
 ;connect to giphy API
 ;generate bear, ninja, or cowboy based on winner
 
-;(defn handler [[ok response]]
-;  (if ok
-;    (.log js/console (str response))
-;    (.error js/console (str response))))
+
+(defn fetch-gif! [winner success-callback]
+  (GET "https://api.giphy.com/v1/gifs/search"
+       {:method          :get
+        :params          {:api_key "FgUBaM6Yua0703Df7Y2I4lrReCPOiw54"
+                          :q       (case winner
+                                     :bear "bear"
+                                     :ninja "ninja"
+                                     :cowboy "cowboy"
+                                     :tie "tie-game")
+                          :limit   1
+                          :offset  0
+                          :rating  "g"
+                          :lang    "en"}
+        :handler         (fn [gif-url]
+                           (success-callback (get-in gif-url [:data 0 :images :downsized_medium :url])))
+        :error-handler   (fn [e] (println "error" e))
+        :response-format :json
+        :keywords?       true}))
+
+
+(comment
+  (fetch-gif! :bear)
+  (fetch-gif! :ninja)
+  (fetch-gif! :cowboy)
+  (def result (atom nil))
+  (fetch-gif! :bear (fn [v]
+                      (reset! result v)
+                      (println "success" v))))
+;@result
+
+;(get-in @result [:data 0 :images :downsized_medium :url])
 ;
-;(defn get-gif (ajax-request
-;                {:uri    "https://api.giphy.com/v1/gifs/search?api_key=FgUBaM6Yua0703Df7Y2I4lrReCPOiw54&q=bear&limit=25&offset=0&rating=g&lang=en"
-;                 :method :get
-;                 :params {:data {:url "https://giphy.com/gifs/bear-hello-waving-IThjAlJnD9WNO"}}
-;                 :handler handler
-;                 :format (:json)
-;                 :response-format (:json {:keywords? true})}))
+;  (count (:data v'))
+;  (first (:data v'))
+;  (keys (first (:data v')))
 ;
-;
-;(defn set-gif-html!
-;  [html-str]
-;  (let [el (gdom/getElement "gif-box row")]
-;    (oset! el "innerHTML" html-str)))
-;
-;(hiccups/defhtml
-;  render-gif [g]
-;  [:div [:class "funny-gif"
-;         [:span [:class "winner-gif"]]]])
-;
-;(defn render-gif! [_ _ prev-val new-val]
-;  (set-gif-html! (render-gif new-val)))
 
