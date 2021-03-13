14387f2819e4296154c10e612758fd2c52b87583
Katie Vogel <kvog983@gmail.com>
2021-03-13 18:33:20 -0500

    games rules swap out for gif
    grame rules swap out for gif. still have an issue with a CSS flash or something

--- src/katie_e_vogel_main/core.cljs
+++ src/katie_e_vogel_main/core.cljs
@@ -2,17 +2,19 @@
   (:require-macros [hiccups.core :as hiccups :refer [html]])
   (:require [katie-e-vogel-main.api-connections :refer [fetch-gif!]]
             [katie-e-vogel-main.game-logic :refer [game-action]]
-            [katie-e-vogel-main.player-chat]
             [katie-e-vogel-main.player-management]
             [katie-e-vogel-main.html]
             [goog.dom :as gdom]
             [oops.core :refer [ocall oget oset!]]))
 
-
+;this should go in the state file
 (defonce state (atom {:p1-choice nil
                       :p2-choice nil
-                      :winner nil}))
+                      :winner nil
+                      :p1-score 0
+                      :p2-score 0}))
 
+;add this to html-cljs file
 (defn click-bnc-button [p1-choice]
   (println "in button" p1-choice)
   (let [p2-choice (rand-nth [:bear :ninja :cowboy])
@@ -24,16 +26,25 @@
                 (fn [v]
                   (swap! state (fn [o]
                                  (assoc o :winner-gif v)))))
-
-
     (swap! state (fn [v]
                    (assoc v
                      :p1-choice p1-choice
                      :p2-choice p2-choice
-                     :winner winner)))))
+                     :winner winner
+                     :p1-score (case winner
+                                 :p1 (inc (:p1-score v 0))
+                                 (:p1-score v))
+                     :p2-score (case winner
+                                 :p2 (inc (:p2-score v 0))
+                                 (:p2-score v)))))))
+
+(comment
+  (score :p1 @state))
+
 
 (comment (game-action :bear :ninja))
 
+;stays in core?
 (defn init-handlers! []
   (let [el (.querySelector js/document ".btn-b")]
     (.addEventListener el "click" (fn [event]
@@ -54,15 +65,15 @@
     (case [p1-choice p2-choice]
       [:bear :ninja] "Bear bites ninja!")))
 
-
+;add this to html-cljs file
 (hiccups/defhtml
-  render-bnc [{:keys [p1-choice p2-choice winner-gif] :as v}]
+  render-bnc [{:keys [p1-choice p2-choice winner-gif p1-score p2-score] :as v}]
+  [:div {:class "jumbotron jumbotron-fluid"}
+   [:div {:class "container"}
+    [:h1 {:class "display-4" } "Bear Ninja Cowboy"]
+    [:p {:class "lead"} "Like Rock Paper Scissors... but better... because bears"]]]
   [:div
-   [:div {:class "jumbotron jumbotron-fluid"}
-    [:div {:class "container"}
-     [:h1 {:class "display-4" } "Bear Ninja Cowboy"]
-     [:p {:class "lead"} "Like Rock Paper Scissors... but better... because bears"]]]
-   [:div {:class "game-container"}
+   [:div {:class "container game-container"}
     [:div {:class "result-box row"}
       [:div {:class "result container-md col-9"} (case [p1-choice p2-choice]
                                                    [:bear :ninja] "Bear bites ninja! Player 1 wins!"
@@ -75,29 +86,44 @@
                                                    [:cowboy :ninja] "Ninja strikes cowboy! Player 2 wins!"
                                                    [:cowboy :cowboy] "It's a tie. Play again."
                                                    nil)]]
-    [:div {:id "gif-box row"}
-     [:div {:class "funny-gif"} [:img {:src winner-gif}]]]
+    [:div {:class "gif-box row"}
+      [:div {:class "card"}
+       [:div {:class "card-header"} "P1 Score"]
+       [:div {:class "card-body"} p1-score]]
+      [:div {:class "funny-gif"}
+       (if winner-gif
+         [:img {:src winner-gif}]
+         [:div [:h3 "Welcome to Bear Ninja Cowboy!"]
+          [:p "It's played just like Rock Paper Scissors, but with bears, ninjas, and cowboys."
+           [:ul {:class "rules"}
+            [:li "Bear beats Ninja. Ninja beats Cowboy. Cowboy beats Bear."]
+            [:li "Click on an image below to select your choice"]
+            [:li "Player 2 (the computer) will pick randomly"]
+            [:li "The winner is listed at the top with a related gif and earns a point."]]]])]
+      [:div {:class "card"}
+       [:div {:class "card-header"} "P2 Score"]
+       [:div {:class "card-body"} p2-score]]]
     [:div {:class "choices-box row"}
-     [:div {:class "choice container-md col-3"} [:button {:type "input" :class "btn-b btn-dark"} [:img {:src "assets/happy-bear.jpg"}]]]
-     [:div {:class "choice container-md col-3"} [:button {:type "input" :class "btn-n btn-dark"} [:img {:src "assets/cat-ninja.jpg"}]]]
-     [:div {:class "choice container-md col-3"} [:button {:type "input" :class "btn-c btn-dark"} [:img {:src "assets/ridiculous-cowboy.jpg"}]]]]]
+     [:div {:class "choice container-md col-3"} [:button {:type "input" :class "btn-b"} [:img {:src "assets/happy-bear.jpg"}]]]
+     [:div {:class "choice container-md col-3"} [:button {:type "input" :class "btn-n"} [:img {:src "assets/cat-ninja.jpg"}]]]
+     [:div {:class "choice container-md col-3"} [:button {:type "input" :class "btn-c"} [:img {:src "assets/ridiculous-cowboy.jpg"}]]]]]
    [:hr]
    [:h1 "debug"]
    [:pre (pr-str v)]])
 
+;stays in core file?
 (defn set-app-html!
   [html-str]
   (let [el (gdom/getElement "appContainer")]
     (oset! el "innerHTML" html-str)
     (init-handlers!)))
 
-(defn render-ui! [_ _ prev-val new-val]
+(defn render-ui! [_ _ _prev-val new-val]
   (set-app-html! (render-bnc new-val)))
 
 (defn init! []
   (add-watch state :render-ui render-ui!)
   (render-ui! nil nil nil @state)
-  #_(add-watch state :render-gif render-gif!)
   (println "running"))
 
 (comment (init!))
