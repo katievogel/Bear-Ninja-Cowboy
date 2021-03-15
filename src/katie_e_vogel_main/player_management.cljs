df3421af699866642a7ad3489190cd44c5f984fd
Katie Vogel <kvog983@gmail.com>
2021-03-15 19:25:57 -0400

    deleted unneeded file

--- src/katie_e_vogel_main/player_management.cljs
+++
@@ -1,8 +0,0 @@
-(ns katie-e-vogel-main.player-management)
-
-;Nice to have
-;first person to open game in browser is player 1
-;second player to open game in browser is player 2
-;next person is 'on deck' player in queue, all non-active players are in ordered queue
-;all players beyond player 2 can spectate
-;when active player leaves, then 'on deck' replaces active player, and queue moves up
