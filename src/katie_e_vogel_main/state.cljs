95d6402717422af6fc6d37dce7677c6fc5616054
Katie Vogel <kvog983@gmail.com>
2021-03-15 19:35:37 -0400

    deleted state file, kept in core

--- src/katie_e_vogel_main/state.cljs
+++
@@ -1,9 +0,0 @@
-(ns katie-e-vogel-main.state
-  (:require [katie-e-vogel-main.player-interaction :as player-interaction]))
-
-(def initial-state
-  {:choice nil
-   :result :not-started
-   :player-state (player-interaction/players)})
-
-(def state (atom initial-state))
